b7e1c0e34de4c8c38b81deeded626c26785e26dc
Metalpunky <Metalpunky@hotmail.fr>
2018-09-03 14:51:24 +0200

    calculatrice v0

---
+++ Calc-ifelse.ps1
@@ -0,0 +1,33 @@
+﻿Clear-Host
+
+
+echo "******************************"
+echo ">>>> Calculatrice Clint © <<<<"
+echo "******************************"
+
+do {
+
+echo ""
+[decimal]$firstnumber = Read-Host  "Premier nombre"
+echo ""
+$operation = Read-Host  "Operation ( + - * / )"
+echo ""
+[decimal]$secondnumber = Read-Host  "Second nombre"
+
+
+
+
+if ($operation -eq "+") {$resultat = $firstnumber + $secondnumber}
+elseif ($operation -eq "-") {$resultat = $firstnumber - $secondnumber}
+elseif ($operation -eq "*") {$resultat = $firstnumber * $secondnumber}
+elseif ($operation -eq "/") {$resultat = $firstnumber / $secondnumber} 
+
+echo ""
+echo "Le résulat est $resultat"
+echo ""
+
+$exit = read-host  "Quitter (O/N) ?"
+clear-host
+
+} while ($exit -like "N")
+
---
+++ Calc-switch.ps1
@@ -0,0 +1,40 @@
+﻿Clear-Host
+
+
+echo "******************************"
+echo ">>>> Calculatrice Clint © <<<<"
+echo "******************************"
+
+do {
+
+echo ""
+[decimal]$firstnumber = Read-Host  "Premier nombre"
+echo ""
+$operation = Read-Host  "Operation ( + - * / )"
+echo ""
+[decimal]$secondnumber = Read-Host  "Second nombre"
+
+
+
+switch ($operation) {
+    "+" {$resultat = $firstnumber + $secondnumber}
+    "-" {$resultat = $firstnumber - $secondnumber}
+    "*" {$resultat = $firstnumber * $secondnumber}
+    "/" {$resultat = $firstnumber / $secondnumber}
+    
+    default {echo "Pas le bon signe d'operation"}
+}
+
+echo ""
+echo "Le résulat est $resultat `a"
+echo ""
+
+$exit = read-host  "Quitter (O/N) ?"
+clear-host
+
+} while ($exit -like "N")
+
+
+# `n renvoi à la ligne , à fait un bip 
+
+
